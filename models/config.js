77143ce928f9a8da4a22d26361cb8f048f2d8c87
alicecampbl <103626922+alicecampbl@users.noreply.github.com>
2025-02-07 22:30:24 +0700

    Update config.js

--- models/config.js
+++ models/config.js
@@ -6,6 +6,7 @@ const config = {
   user: process.env.PG_USER_PROD,
   password: process.env.PG_PASSWORD_PROD,
   database: process.env.PG_DATABASE_PROD,
+  connectionString: process.env.PG_DATABASE_URI,
 };
 
 const pool = process.env.NODE_ENV === "prod" ? new Pool(config) : new Pool();
