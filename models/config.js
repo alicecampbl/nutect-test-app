bcdc8b0544b3816d60353816888d2d101f465981
alicecampbl <103626922+alicecampbl@users.noreply.github.com>
2025-02-07 22:03:30 +0700

    Update config.js

--- models/config.js
+++ models/config.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
 
 const config = {
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  host: process.env.PG_HOST_PROD,
+  port: process.env.PG_PORT_PROD,
+  user: process.env.PG_USER_PROD,
+  password: process.env.PG_PASSWORD_PROD,
+  database: process.env.PG_DATABASE_PROD,
 };
 
-const pool = new Pool(config);
+const pool = process.env.NODE_ENV === "prod" ? new Pool(config) : new Pool();
 
 module.exports = pool;
